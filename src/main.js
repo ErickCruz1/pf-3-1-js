6068bdcf688ad4d13756b6dfc074dd03d872d48c
Erick Cruz <erick.cruz.dev1@gmail.com>
2026-05-13 12:03:15 -0600

    Laboratorio

--- src/main.js
+++ src/main.js
@@ -1,4 +1,29 @@
 
-export function addTwoNumbers(/*aqui tambien va código*/){
+export function addTwoNumbers(a, b){
   //aqui va tu código
+  console.log(a + b)
+
 }
+
+function minus(a, b) {
+  return a - b
+}
+
+function Division(a, b) {
+  return a / b
+}
+
+function addThreeNumbers(a, b, c){
+  return a + b + c
+}
+
+function A(x){
+  function B(y){
+    function C(z){
+     (x + y - z );
+    }
+    C(3);
+  }
+  B(2);
+}
+A(1);
